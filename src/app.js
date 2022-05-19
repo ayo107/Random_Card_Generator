f8e66f021daf8ce4b319cdba34c19fc92001b994
ayo107 <ayo_linkin10@hotmail.com>
2022-05-19 16:30:09 +0000

    primera subida  random card

--- src/app.js
+++ src/app.js
@@ -5,7 +5,115 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const palos = ["corazon", "diamante", "pica", "trebol"];
+const valores = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let paloArriba = document.querySelector("#simboloArriba");
+let numero = document.querySelector("#numero");
+let paloAbajo = document.querySelector("#simboloAbajo");
+
+function cartaEllection(arr1, arr2) {
+  let carta = [];
+  carta.push(arr1[Math.floor(Math.random() * arr1.length)]);
+  carta.push(arr2[Math.floor(Math.random() * arr2.length)]);
+  return carta;
+}
+
+function colorEllection(arr) {
+  if (arr[0] === "corazon" || arr[0] === "diamante") {
+    paloArriba.style.color = "red";
+    numero.style.color = "red";
+    paloAbajo.style.color = "red";
+  } else {
+    paloArriba.style.color = "black";
+    numero.style.color = "black";
+    paloAbajo.style.color = "black";
+  }
+}
+function asignarPalo(arr) {
+  if (arr[0] === "corazon") {
+    paloArriba.innerHTML = "♥";
+    paloAbajo.innerHTML = "♥";
+  } else if (arr[0] === "diamante") {
+    paloArriba.innerHTML = "♦";
+    paloAbajo.innerHTML = "♦";
+  } else if (arr[0] === "trebol") {
+    paloArriba.innerHTML = "♣";
+    paloAbajo.innerHTML = "♣";
+  } else {
+    paloArriba.innerHTML = "♠";
+    paloAbajo.innerHTML = "♠";
+  }
+}
+function asignarValor(arr) {
+  numero.innerHTML = arr[1];
+}
+function generacionCarta() {
+  let carta = cartaEllection(palos, valores);
+  colorEllection(carta);
+  asignarPalo(carta);
+  asignarValor(carta);
+}
+
+window.onload = function baraja() {
+  generacionCarta();
 };
+document.querySelector(".button").addEventListener("click", generacionCarta);
+
+var segundos = document.getElementById("segundos");
+var boton = document.getElementById("btnCuentaAtras");
+var cuentaAtras = document.getElementById("cuentaAtras");
+var notificacion = document.getElementById("notificacion");
+
+segundos.focus();
+
+var countDown = function() {
+  notificacion.classList.remove("alert");
+  notificacion.classList.remove("alert-success");
+  notificacion.textContent = "";
+
+  cuentaAtras.style.color = "black";
+
+  var contador = segundos.value;
+  cuentaAtras.textContent = contador;
+
+  var valorCuentaAtras = setInterval(function() {
+    if (contador > 0) {
+      contador--;
+      cuentaAtras.textContent = contador;
+    } else {
+      clearInterval(valorCuentaAtras);
+      notificacion.textContent = "Fin de la cuenta atrás";
+      notificacion.classList.add("alert-success");
+      notificacion.classList.add("alert");
+      segundos.value = "";
+      //segundos.focus();
+    }
+
+    if (contador <= 3) {
+      cuentaAtras.style.color = "violet";
+    }
+    if (contador === 1) {
+      cuentaAtras.textContent = "Fin de la cuenta atrás";
+    }
+    if (contador === 0) {
+      cuentaAtras.textContent = "Fin de la cuenta atrás";
+      location.reload();
+    }
+  }, 1000);
+};
+
+boton.addEventListener("click", countDown);
